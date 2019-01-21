aba4aac253f7c534e77408ab5225fe157126bdf3
S0mbr3 <benjamin_denis@outlook.com>
2019-01-21 19:02:26 +0100

    creation of MainController, using the Humor classed with reflexivity to to detect and display the humor in an Arraylist

--- app/src/main/java/com/s0mbr3/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/s0mbr3/moodtracker/controller/MainActivity.java
@@ -7,7 +7,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.s0mbr3.moodtracker.R;
+import com.s0mbr3.moodtracker.controller.MainControllers.MainController;
 import com.s0mbr3.moodtracker.model.Humor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mSmiley = findViewById(R.id.activity_main_smiley_image);
-        Humor humor = new Humor(mSmiley);
-        humor.getSadSmiley();
+        try {
+            MainController mainController = new MainController(mSmiley);
+            mainController.getMethodName();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         //mSmiley.setImageResource(R.drawable.smiley_disappointed);
 
     }
--- app/src/main/java/com/s0mbr3/moodtracker/model/Humor.java
+++ app/src/main/java/com/s0mbr3/moodtracker/model/Humor.java
@@ -13,10 +13,31 @@ public class Humor {
 
     public Humor(ImageView smiley){
         this.mSmiley = smiley;
+
     }
 
     public ImageView getSadSmiley() {
         this.mSmiley.setImageResource(R.drawable.smiley_sad);
+        return mSmiley;
+    }
+
+    public ImageView getDisappointedSmiley(){
+        this.mSmiley.setImageResource(R.drawable.smiley_disappointed);
+        return mSmiley;
+    }
+
+    public ImageView getNormalSmiley(){
+       this.mSmiley.setImageResource(R.drawable.smiley_normal);
+        return mSmiley;
+    }
+
+    public ImageView getHappySmiley(){
+       this.mSmiley.setImageResource(R.drawable.smiley_happy);
+        return mSmiley;
+    }
+
+    public ImageView getSuperHappySmiley(){
+       this.mSmiley.setImageResource(R.drawable.smiley_super_happy);
         return mSmiley;
     }
 
