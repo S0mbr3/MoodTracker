fe33d1bad997d357393ab5a6ad35f418f3789070
S0mbr3 <benjamin_denis@outlook.com>
2019-01-21 22:08:08 +0100

    fixing users experience by adding a boolean tester when the the humor has been changed

--- app/src/main/java/com/s0mbr3/moodtracker/controller/MainControllers/MyGestureListener.java
+++ app/src/main/java/com/s0mbr3/moodtracker/controller/MainControllers/MyGestureListener.java
@@ -13,11 +13,13 @@ public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
     private ImageView mSmiley;
     private MainController mainController;
     private int mIndex;
+    private boolean changed;
 
     public MyGestureListener(ImageView smiley, int index){
         this.mSmiley = smiley;
         this.mainController = new MainController(smiley);
         this.mIndex = index;
+        this.changed = false;
 
         mainController.getMethodName(3);
     }
@@ -34,12 +36,26 @@ public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
         //return super.onScroll(e1, e2, distanceX, distanceY);
         Log.d(DEBUG_TAG, "onScroll " + distanceX + " " + distanceY);
-        if (distanceY < 0 && this.mIndex > 0){
-
-            mainController.getMethodName(this.mIndex--);
-        } else if (distanceY > 0 && this.mIndex < 4){
-            mainController.getMethodName(this.mIndex++);
+        if (distanceY < 0 && this.changed == false &&  this.mIndex >= 0){
+            this.changed = true;
+            if (mIndex != 0) this.mIndex--;
+            Log.d(DEBUG_TAG, String.valueOf(this.mIndex));
+            mainController.getMethodName(this.mIndex);
+        } else if (distanceY > 0 && this.changed == false && this.mIndex <= 4){
+            if (mIndex != 4) this.mIndex++;
+            Log.d(DEBUG_TAG, String.valueOf(this.mIndex));
+            mainController.getMethodName(this.mIndex);
+            this.changed = true;
         }
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        //return super.onFling(e1, e2, velocityX, velocityY);
+        this.changed = false;
+        Log.d(DEBUG_TAG, "onFling");
+
         return true;
     }
 }
